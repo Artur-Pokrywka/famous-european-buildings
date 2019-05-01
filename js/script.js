f5154fc55ad3a9cc0298000024de30f0f31cad8b
Artur Pokrywka <pokrywkaart@gmail.com>
2019-05-01 19:53:24 +0200

    Add carousel with photos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log("hello")
+"use strict";
+
+var elem = document.querySelector('.main-carousel');
+var restartButton = document.getElementById("btn-restart");
+var progressBar = document.querySelector('.progress-bar');
+
+var flkty = new Flickity( elem, { 
+  cellAlign: 'left',
+  contain: true,
+  hash: true
+});
+
+function goToBeginning() {
+    flkty.select( 0, false, true );
+}
+restartButton.addEventListener ("click", goToBeginning);
+
+flkty.on( 'scroll', function(progress) {
+    progress = Math.max( 0, Math.min( 1, progress ) );
+    progressBar.style.width = progress * 100 + '%';
+});
+
+// element argument can be a selector string
+//   for an individual element
+
+
+// var flkty = new Flickity( '.main-carousel', {
+//   options
+// });
