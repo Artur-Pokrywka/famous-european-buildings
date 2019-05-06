88393a03de85a9605952481d59dfea00579c3d5e
Artur Pokrywka <pokrywkaart@gmail.com>
2019-05-06 21:20:07 +0200

    Add functions for adding new markers

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 var elem = document.querySelector('.main-carousel');
 var restartButton = document.getElementById("btn-restart");
 var progressBar = document.querySelector('.progress-bar');
+var infos = document.getElementById("infos");
+var mapCoordinates = [];
 
 var carouselContainer = document.getElementById("main-carousel");
 var carouselElement = document.getElementById("cities-array-element").innerHTML;
@@ -12,27 +14,32 @@ var citiesArray = [
             {
                 imageSrc: "https://images.pexels.com/photos/87374/pexels-photo-87374.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
                 title: "Louvre Museum",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                coords: {lat: 48.85661, lng: 2.351499}
             },
             {
                 imageSrc: "https://images.pexels.com/photos/1114892/pexels-photo-1114892.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
                 title: "Brandenburg Gate",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                coords: {lat: 52.5163 , lng: 13.3777}
             },
             {
                 imageSrc: "https://images.pexels.com/photos/1243538/pexels-photo-1243538.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
                 title: "Papal Basilica of St. Peter ",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                coords: {lat: 41.9022 , lng: 12.4537}
             },
             {
                 imageSrc: "https://images.pexels.com/photos/42061/architecture-building-city-dark-42061.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
                 title: "London Bridge ",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                coords: {lat: 51.508 , lng: -0.087682}
             },
             {
                 imageSrc: "https://images.pexels.com/photos/83133/night-83133.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
                 title: "Wilanow Palace ",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                coords: {lat: 52.164089 , lng: 21.088153}
             }
 ];
 
@@ -40,8 +47,7 @@ function carouselBuilder() {
   for (var i = 0; i < citiesArray.length; i++ ){
     var div = document.createElement("div");
     div = Mustache.render(carouselElement, citiesArray[i]); 
-    carouselContainer.insertAdjacentHTML('beforeend', div);   
-    // console.log(div);  
+    carouselContainer.insertAdjacentHTML('beforeend', div);     
   }
 }
 carouselBuilder();
@@ -62,114 +68,131 @@ flkty.on( 'scroll', function(progress) {
     progressBar.style.width = progress * 100 + '%';
 });
 
-var infos = document.getElementById("infos");
+
 // Initialize and add the map
 window.initMap = function() {
     // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
-    var sydney = {lat: -33.874237, lng: 151.198517};;
+    
+    function createArraywWithCooridinates() {
+        for (var i = 0; i < citiesArray.length; i++) {   
+        var mapCoordinate = citiesArray[i].coords;
+        mapCoordinates.push(mapCoordinate);
+        };  
+    };
+    createArraywWithCooridinates();
    
+    // var markersArray = [];
+    var marker;
+    function addMarkersForCoordinates() {
+        for(var i = 0; i < mapCoordinates.length; i++) {
+            var mapCoordinate = mapCoordinates[i];
+            var marker = new google.maps.Marker({
+                position: mapCoordinate, 
+                map: map
+            });
+            // markersArray.push(marker);
+            console.log(marker);
+        } 
+    };
+    addMarkersForCoordinates();
+    
+    var paris = mapCoordinates[0];
+    
     // The map, centered at Uluru
     var map = new google.maps.Map(
         document.getElementById('map'), {
             zoom: 7, 
-            center: uluru
+            center: paris
         });
     // The marker, positioned at Uluru
     var markerOne = new google.maps.Marker({
-        position: uluru, 
+        position: paris, 
         map: map
     });
+
     markerOne.addListener("click", function(){
         infos.innerHTML = "You clicked markerOne";
     });
 
-    var markerTwo = new google.maps.Marker({
-        position: sydney, 
-        map: map
-    });
-
     document.getElementById("center-map").addEventListener("click", function(event) {
         event.preventDefault();
-        map.panTo(sydney);
+        map.panTo(paris);
         map.setZoom(10);
     });
 
-    document.getElementById("center-smooth").addEventListener("click", function(event) {
-        event.preventDefault();
-        smoothPanAndZoom(map, 7, uluru);
-    });
-
+    // document.getElementById("center-smooth").addEventListener("click", function(event) {
+    //     event.preventDefault();
+    //     smoothPanAndZoom(map, 7, paris);
+    // });
     
-    var smoothPanAndZoom = function(map, zoom, coords){
-		// Trochę obliczeń, aby wyliczyć odpowiedni zoom do którego ma oddalić się mapa na początku animacji.
-		var jumpZoom = zoom - Math.abs(map.getZoom() - zoom);
-		jumpZoom = Math.min(jumpZoom, zoom -1);
-		jumpZoom = Math.max(jumpZoom, 3);
+    // var smoothPanAndZoom = function(map, zoom, coords){
+	// 	// Trochę obliczeń, aby wyliczyć odpowiedni zoom do którego ma oddalić się mapa na początku animacji.
+	// 	var jumpZoom = zoom - Math.abs(map.getZoom() - zoom);
+	// 	jumpZoom = Math.min(jumpZoom, zoom -1);
+	// 	jumpZoom = Math.max(jumpZoom, 3);
 
-		// Zaczynamy od oddalenia mapy do wyliczonego powiększenia. 
-		smoothZoom(map, jumpZoom, function(){
-			// Następnie przesuwamy mapę do żądanych współrzędnych.
-			smoothPan(map, coords, function(){
-				// Na końcu powiększamy mapę do żądanego powiększenia. 
-				smoothZoom(map, zoom); 
-			});
-		});
-	};
+	// 	// Zaczynamy od oddalenia mapy do wyliczonego powiększenia. 
+	// 	smoothZoom(map, jumpZoom, function(){
+	// 		// Następnie przesuwamy mapę do żądanych współrzędnych.
+	// 		smoothPan(map, coords, function(){
+	// 			// Na końcu powiększamy mapę do żądanego powiększenia. 
+	// 			smoothZoom(map, zoom); 
+	// 		});
+	// 	});
+	// };
 	
-	var smoothZoom = function(map, zoom, callback) {
-		var startingZoom = map.getZoom();
-		var steps = Math.abs(startingZoom - zoom);
-		
-		// Jeśli steps == 0, czyli startingZoom == zoom
-		if(!steps) {
-			// Jeśli podano trzeci argument
-			if(callback) {
-				// Wywołaj funkcję podaną jako trzeci argument.
-				callback();
-			}
-			// Zakończ działanie funkcji
-			return;
-		}
+	// var smoothZoom = function(map, zoom, callback) {
+	// 	var startingZoom = map.getZoom();
+	// 	var steps = Math.abs(startingZoom - zoom);
+	// 	// Jeśli steps == 0, czyli startingZoom == zoom
+	// 	if(!steps) {
+	// 		// Jeśli podano trzeci argument
+	// 		if(callback) {
+	// 			// Wywołaj funkcję podaną jako trzeci argument.
+	// 			callback();
+	// 		}
+	// 		// Zakończ działanie funkcji
+	// 		return;
+	// 	}
 
-		// Trochę matematyki, dzięki której otrzymamy -1 lub 1, w zależności od tego czy startingZoom jest mniejszy od zoom
-		var stepChange = - (startingZoom - zoom) / steps;
+	// 	// Trochę matematyki, dzięki której otrzymamy -1 lub 1, w zależności od tego czy startingZoom jest mniejszy od zoom
+	// 	var stepChange = - (startingZoom - zoom) / steps;
 
-		var i = 0;
-		// Wywołujemy setInterval, który będzie wykonywał funkcję co X milisekund (X podany jako drugi argument, w naszym przypadku 80)
-		var timer = window.setInterval(function(){
-			// Jeśli wykonano odpowiednią liczbę kroków
-			if(++i >= steps) {
-				// Wyczyść timer, czyli przestań wykonywać funkcję podaną w powyższm setInterval
-				window.clearInterval(timer);
-				// Jeśli podano trzeci argument
-				if(callback) {
-					// Wykonaj funkcję podaną jako trzeci argument
-					callback();
-				}
-			}
-			// Skorzystaj z metody setZoom obiektu map, aby zmienić powiększenie na zaokrąglony wynik poniższego obliczenia
-			map.setZoom(Math.round(startingZoom + stepChange * i));
-		}, 80);
-	};
+	// 	var i = 0;
+	// 	// Wywołujemy setInterval, który będzie wykonywał funkcję co X milisekund (X podany jako drugi argument, w naszym przypadku 80)
+	// 	var timer = window.setInterval(function(){
+	// 		// Jeśli wykonano odpowiednią liczbę kroków
+	// 		if(++i >= steps) {
+	// 			// Wyczyść timer, czyli przestań wykonywać funkcję podaną w powyższm setInterval
+	// 			window.clearInterval(timer);
+	// 			// Jeśli podano trzeci argument
+	// 			if(callback) {
+	// 				// Wykonaj funkcję podaną jako trzeci argument
+	// 				callback();
+	// 			}
+	// 		}
+	// 		// Skorzystaj z metody setZoom obiektu map, aby zmienić powiększenie na zaokrąglony wynik poniższego obliczenia
+	// 		map.setZoom(Math.round(startingZoom + stepChange * i));
+	// 	}, 80);
+	// };
 
-	// Poniższa funkcja działa bardzo podobnie do smoothZoom. Spróbuj samodzielnie ją przeanalizować. 
-	var smoothPan = function(map, coords, callback) {
-		var mapCenter = map.getCenter();
-		coords = new google.maps.LatLng(coords);
+	// // Poniższa funkcja działa bardzo podobnie do smoothZoom. Spróbuj samodzielnie ją przeanalizować. 
+	// var smoothPan = function(map, coords, callback) {
+	// 	var mapCenter = map.getCenter();
+	// 	coords = new google.maps.LatLng(coords);
 
-		var steps = 12;
-		var panStep = {lat: (coords.lat() - mapCenter.lat()) / steps, lng: (coords.lng() - mapCenter.lng()) / steps};
+	// 	var steps = 12;
+	// 	var panStep = {lat: (coords.lat() - mapCenter.lat()) / steps, lng: (coords.lng() - mapCenter.lng()) / steps};
 
-		var i = 0;
-		var timer = window.setInterval(function(){
-			if(++i >= steps) {
-				window.clearInterval(timer);
-				if(callback) callback();
-			}
-			map.panTo({lat: mapCenter.lat() + panStep.lat * i, lng: mapCenter.lng() + panStep.lng * i});
-		}, 1000/30);
-	}; 
+	// 	var i = 0;
+	// 	var timer = window.setInterval(function(){
+	// 		if(++i >= steps) {
+	// 			window.clearInterval(timer);
+	// 			if(callback) callback();
+	// 		}
+	// 		map.panTo({lat: mapCenter.lat() + panStep.lat * i, lng: mapCenter.lng() + panStep.lng * i});
+	// 	}, 1000/30);
+	// }; 
   }
 
 	
